c3b51bf2dc4c9b7befd7faf96a29e6632da0059e
Gabriel <gabriel.leite.leandro@gmail.com>
2023-01-04 19:05:34 -0300

    feat: signin & signup routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js"
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(userRoutes);
 
 const port = process.env.PORT;
 
