64e0736cc98d5ea7f386ac2cab92192870b177e8
caiohbe <caiohbe@gmail.com>
2023-01-05 14:37:24 -0300

    fix: send id by query string

--- src/repositories/user.repositories.js
+++ src/repositories/user.repositories.js
@@ -2,7 +2,7 @@ import { connectionDB } from "../database/db.js"
 
 export function getUsersByName(name) {
     return connectionDB.query(
-        "SELECT name, photo FROM users WHERE name ILIKE $1;",
+        "SELECT name, photo, id FROM users WHERE name ILIKE $1;",
         [name]
     )
 }
