1f9e9b7f06c1046c49addd927ddb8648e920369b
BrVecchi <bno.vecchi@gmail.com>
2023-01-05 14:50:41 -0300

    fix: delete post controller

--- src/controllers/delete.post.controller.js
+++ src/controllers/delete.post.controller.js
@@ -6,11 +6,24 @@ export const deletePost = async (req, res) => {
   const token = req.token;
 
   try {
-    const userId = (await connectionDB.query(`SELECT user_id FROM sessions WHERE token = $1`, [token])).rows[0].user_id
-    const userValidation = (await connectionDB.query(`SELECT user_id FROM posts WHERE id = $1`, [postId])).rows[0].user_id
+    const userId = (
+      await connectionDB.query(
+        `SELECT user_id FROM sessions WHERE token = $1`,
+        [token]
+      )
+    ).rows[0].user_id;
+    const userValidation = (
+      await connectionDB.query(`SELECT user_id FROM posts WHERE id = $1`, [
+        postId,
+      ])
+    ).rows[0].user_id;
+    
     if (userId !== userValidation) {
-        return res.sendStatus(401)
+      return res.sendStatus(401);
     }
+    await connectionDB.query(`DELETE FROM posts_hashtags WHERE post_id = $1`, [
+      postId,
+    ]);
     await connectionDB.query(`DELETE FROM posts WHERE id = $1`, [postId]);
     res.sendStatus(204);
   } catch (error) {
