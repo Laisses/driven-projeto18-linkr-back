237b4bf0dff92609d8a3125592f6c4b15b84cfa3
Esdras Antero <esdrasanterofontes@gmail.com>
2023-01-05 02:42:12 -0300

    feat: hashtag post route

--- src/controllers/hashtag.controllers.js
+++ src/controllers/hashtag.controllers.js
@@ -1,4 +1,4 @@
-import { getTrendingList } from "../repositories/hashtag.repositories.js"
+import { getHashtagFeed, getTrendingList } from "../repositories/hashtag.repositories.js"
 
 export async function trendingHashtagsControl (req, res) {
     try {
@@ -9,6 +9,23 @@ export async function trendingHashtagsControl (req, res) {
           }
       
         return res.status(200).send(trendingList.rows);
+    } catch (error) {
+        console.log(error.message)
+        return res.sendStatus(500)
+    }
+}
+
+export async function feedHashtagControl (req, res) {
+    const { hashtag } = req.params
+
+    try {
+        const feed = await getHashtagFeed(hashtag)
+
+        if (feed.rowCount === 0) {
+            return res.sendStatus(404)
+          }
+      
+        return res.status(200).send(feed.rows);
     } catch (error) {
         console.log(error.message)
         return res.sendStatus(500)
