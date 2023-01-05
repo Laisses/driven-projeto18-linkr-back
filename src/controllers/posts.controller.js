88317c494ffb5bb3fede1ff166f225084ac9ae8b
Laisses <laisse@gmail.com>
2023-01-05 17:11:09 -0300

    feat: add metadata scraper

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,4 +1,5 @@
 import * as r from "../repositories/posts.repositories.js";
+import getMetaData from "metadata-scraper";
 
 export const readPosts = async (req, res) => {
     const posts = await r.getAllPosts();
@@ -13,7 +14,9 @@ export const postLink = async (req, res) => {
 
     const user_id = (await r.getUser(token)).rows[0].user_id;
     const post_id = (await r.addNewPost(user_id, description)).rows[0].id;
+    const data = await getMetaData(link);
 
+    console.log(data);
 
     res.sendStatus(200);
 };
