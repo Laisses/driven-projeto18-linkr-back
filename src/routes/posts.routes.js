c7b24e68e1985b4b941af12c13ce23e1aff35785
Laisses <laisse@gmail.com>
2023-01-07 17:54:44 -0300

    feat: edit description route

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -7,6 +7,7 @@ import { tokenMiddleware } from "../middlewares/token.validation.middleware.js";
 
 export const postsRouter = Router();
 
-postsRouter.delete("/timeline", tokenMiddleware, deletePost)
 postsRouter.get("/timeline", tokenMiddleware, m.asyncError(c.readPosts));
 postsRouter.post("/timeline", tokenMiddleware, m.validate(s.post), m.asyncError(c.postLink));
+postsRouter.put("/timeline", tokenMiddleware, m.validate(s.newDescription), m.validatePostEdit, m.asyncError(c.editDescription));
+postsRouter.delete("/timeline", tokenMiddleware, deletePost);
