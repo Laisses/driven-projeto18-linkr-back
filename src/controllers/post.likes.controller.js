9e9859b5019e64af4e030e4a0771d7a03f41e383
BrVecchi <bno.vecchi@gmail.com>
2023-01-13 17:12:13 -0300

    refactor: arquitecture post likescontroller

--- src/controllers/post.likes.controller.js
+++ src/controllers/post.likes.controller.js
@@ -1,43 +1,27 @@
-import { connectionDB } from "../database/db.js";
+import {findUser, findLike, insertLike, deleteLike} from "../repositories/likes.repositories.js"
 
 export const postLikes = async (req, res) => {
+  try {
   const token = req.token;
   const { id } = req.body;
   const postId = id;
-
+    
   if(!postId) {
     return sendStatus(404);
   }
 
-  try {
-    const userId = (
-      await connectionDB.query(
-        `SELECT user_id FROM sessions WHERE token = $1`,
-        [token]
-      )
-    ).rows[0].user_id;
+    const userId = await findUser(token);
 
-    const likeId = (
-      await connectionDB.query(
-        `SELECT * FROM posts_likes WHERE user_id = $1 AND post_id = $2`,
-        [userId, postId]
-      )
-    ).rows[0];
+    const likeId = await findLike(userId, postId);
 
     {
       !likeId
-        ? await connectionDB.query(
-            `INSERT INTO posts_likes (post_id, user_id) VALUES ($1, $2) `,
-            [postId, userId]
-          )
-        : await connectionDB.query(
-            `DELETE FROM posts_likes WHERE id = $1`,
-            [likeId.id]
-          );
+        ? await insertLike(postId, userId)
+        : await deleteLike(likeId)
     }
     res.sendStatus(201);
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
     res.sendStatus(500);
   }
 };
